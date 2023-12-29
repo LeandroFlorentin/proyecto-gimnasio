43113f863236261324e05fc1c09d0e44c3be39bd
unknown <leandro.florentin@hotmail.com>
2023-12-28 23:36:41 -0300

    front

--- client/src/components/estructura/Accordion/AccordionGym.tsx
+++ client/src/components/estructura/Accordion/AccordionGym.tsx
@@ -1,7 +1,7 @@
 import { Accordion } from "react-bootstrap";
 export default function AccordionGym({ eventKey, activateKey, key, title, children }: any) {
   return (
-    <Accordion defaultActiveKey={activateKey} key={key}>
+    <Accordion key={key}>
       <Accordion.Item eventKey={eventKey}>
         <Accordion.Header>{title}</Accordion.Header>
         <Accordion.Body>{children}</Accordion.Body>
